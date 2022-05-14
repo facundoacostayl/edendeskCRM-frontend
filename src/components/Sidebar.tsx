8a44a76eeff9399752ad215359fc86b8e5d5bf70
Facundo Acosta <facundoacostayl@outlook.com>
2022-05-14 07:11:47 -0300

    Links added to sidebar icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,27 +1,48 @@
-//SIDEBAR COMPONENTS
+//UTILS
+import { Link } from "react-router-dom";
+
+//COMPONENTS
 import { SidebarContainer } from "../ui/sidebar/SidebarContainer";
 import { MenuList } from "../ui/sidebar/MenuList";
-import {Icon, UserIcon } from "../ui/icons";
+import { Icon, UserIcon } from "../ui/icons";
 
 //ICONS
-import {DashboardIcon, MyClientsIcon, NewBalanceIcon, NewClientIcon, MenuIcon} from '../ui/icons';
+import {
+  DashboardIcon,
+  MyClientsIcon,
+  NewBalanceIcon,
+  NewClientIcon,
+  MenuIcon,
+} from "../ui/icons";
 
 export const Sidebar = () => {
   return (
     <SidebarContainer>
-        {/*<Hamburguer />*/}
-        <div className="">
-        <img className="hidden md:block" src={require('../img/logo.png')} alt="" />
+      {/*<Hamburguer />*/}
+      <div className="">
+        <img
+          className="hidden md:block"
+          src={require("../img/logo.png")}
+          alt=""
+        />
         <MenuList>
-          <Icon type={DashboardIcon}/>
-          <Icon type={MyClientsIcon}></Icon>
-          <Icon type={NewBalanceIcon}></Icon>
-          <Icon type={NewClientIcon}></Icon>
+          <Link to="/dashboard" className="w-full">
+            <Icon type={DashboardIcon} />
+          </Link>
+          <Link to="/mis-clientes" className="w-full">
+            <Icon type={MyClientsIcon} />
+          </Link>
+          <Link to="/nuevo-saldo" className="w-full">
+            <Icon type={NewBalanceIcon} />
+          </Link>
+          <Link to="/nuevo-cliente" className="w-full">
+            <Icon type={NewClientIcon} />
+          </Link>
         </MenuList>
-        </div>
-        <div className="hidden w-full md:block">
-        <UserIcon/>
-        </div>
-      </SidebarContainer>
-  )
-}
+      </div>
+      <div className="hidden w-full md:block">
+        <UserIcon />
+      </div>
+    </SidebarContainer>
+  );
+};
--- src/ui/icons/UserIcon.tsx
+++ src/ui/icons/UserIcon.tsx
@@ -1,5 +1,8 @@
+//HOOKS
 import { useState } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+//COMPONENTS
 import { FontAwesomeIcon, MyProfileIcon } from "../icons";
 
 type Props = {
@@ -11,15 +14,25 @@ export const UserIcon = () => {
 
   return (
     <>
-    {isMenuActive && 
-    <div onClick={() => setIsMenuActive(false)} className="absolute top-0 left-0 w-screen h-screen">
-    <div className="absolute xsm:top-0 right-0 md:bottom-0 md:left-0 w-[150px] h-[100px] flex items-center border border-gray-200 text-gray-700 bg-white rounded-md">
-        <ul className="w-full">
-          <Link to="/mi-perfil"><li className="w-full py-1 px-2 text-lg font-medium hover:bg-gray-200">Mi Perfil</li></Link>
-          <li className="w-full py-1 px-2 text-lg font-medium cursor-pointer hover:bg-gray-200">Cerrar Sesión</li>
-        </ul>
-    </div>
-    </div>}
+      {isMenuActive && (
+        <div
+          onClick={() => setIsMenuActive(false)}
+          className="absolute top-0 left-0 w-screen h-screen"
+        >
+          <div className="absolute xsm:top-0 right-0 md:bottom-0 md:left-0 w-[150px] h-[100px] flex items-center border border-gray-200 text-gray-700 bg-white rounded-md">
+            <ul className="w-full">
+              <Link to="/mi-perfil">
+                <li className="w-full py-1 px-2 text-lg font-medium hover:bg-gray-200">
+                  Mi Perfil
+                </li>
+              </Link>
+              <li className="w-full py-1 px-2 text-lg font-medium cursor-pointer hover:bg-gray-200">
+                Cerrar Sesión
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
       <li
         onClick={() => setIsMenuActive(true)}
         className={`md:w-full md:flex md:h-[50px] items-center justify-center list-none cursor-pointer hover:bg-gray-200`}
