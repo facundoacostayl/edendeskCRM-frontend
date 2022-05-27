7fd947d3439c63f34b099aaeb780c8db7bfa9dd0
Facundo Acosta <facundoacostayl@outlook.com>
2022-05-27 07:51:45 -0300

    MyProfile updating values functions updated

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -1,5 +1,5 @@
 //HOOKS
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
 import { useAuth } from "../authContext/AuthProvider";
 
 import { Sidebar } from "../components/Sidebar";
@@ -10,11 +10,63 @@ import { Button } from "../ui/controls/button";
 
 //TYPES
 import { Status } from "../types";
+import { User } from "../authContext/types";
+
+interface Form extends React.FormEvent<HTMLFormElement> {
+  loginemail: HTMLInputElement;
+  password: HTMLInputElement;
+}
+
+type UserData = {
+  loginemail?: User["loginemail"];
+  password?: User["password"];
+};
 
 export const MyProfile = () => {
   const { userData } = useAuth();
   const [status, setStatus] = useState<Status>(Status.init);
   const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  const updateUser = async (newUserData: UserData) => {
+    try {
+      const body = newUserData;
+      const id = userData.id;
+
+      const response = await fetch(`http://localhost:4000/user/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newUserData),
+      });
+
+      const parseRes = await response.json();
+      console.log(parseRes);
+    } catch (error) {
+      error instanceof Error && console.error(error);
+    }
+  };
+
+  const onUpdateValue = (e: Form) => {
+    e.preventDefault();
+
+    const email = e.currentTarget.loginemail;
+    const password = e.currentTarget.password;
+    const rPassword = e.currentTarget.rPassword;
+
+    if (!email.value && !password.value) return;
+
+    if (email.value && !password.value) {
+      updateUser({ loginemail: email.value });
+      email.value = "";
+      return;
+    }
+
+    if (password.value && !email.value && password.value === rPassword.value) {
+      updateUser({password: password.value});
+      password.value = "";
+      rPassword.value = "";
+      return;
+    }
+  };
 
   useEffect(() => {
     setStatus(Status.success);
@@ -25,7 +77,7 @@ export const MyProfile = () => {
     <div className="md:flex">
       <Sidebar />
       <div className="w-full">
-      <SectionBanner sectionName="Mis Datos" />
+        <SectionBanner sectionName="Mis Datos" />
         <PageContent direction="flex-col" status={status}>
           <h2 className="text-2xl font-semibold mt-3">Datos del usuario</h2>
           <div className="w-full my-1 py-1 px-1">
@@ -37,25 +89,37 @@ export const MyProfile = () => {
                 {userData.loginemail}
               </p>
             </div>
-            <form>
+            <form onSubmit={onUpdateValue}>
               <div className="mb-5">
                 <label htmlFor="userName-info">Nuevo correo</label>
-                <TextField placeholder={userData.loginemail} />
+                <TextField
+                  name="loginemail"
+                  placeholder={userData.loginemail}
+                />
+              </div>
+
+              <h3 className="text-lg font-semibold my-4">Cambiar contraseña</h3>
+
+              <div className="mb-3">
+                <label htmlFor="userName-info">Nueva contraseña</label>
+                <TextField
+                  name="password"
+                  type="password"
+                  placeholder="*******"
+                />
+              </div>
+              <div className="mb-5">
+                <label htmlFor="userName-info">Repetir contraseña</label>
+                <TextField
+                  name="rPassword"
+                  type="password"
+                  placeholder="*******"
+                />
+              </div>
+              <div className="w-2/4 mx-auto flex justify-center">
+                <Button colorScheme="secondary">Editar</Button>
               </div>
             </form>
-
-            <h3 className="text-lg font-semibold my-4">Cambiar contraseña</h3>
-            <div className="mb-3">
-              <label htmlFor="userName-info">Nueva contraseña</label>
-              <TextField type="password" placeholder="*******" />
-            </div>
-            <div className="mb-5">
-              <label htmlFor="userName-info">Repetir contraseña</label>
-              <TextField type="password" placeholder="*******" />
-            </div>
-            <div className="flex justify-center border border-gray-500">
-              <Button colorScheme="secondary">Editar</Button>
-            </div>
           </div>
         </PageContent>
       </div>
