8ba6006f645234053887940fe5ab9a16455de99b
Facundo Acosta <facundoacostayl@outlook.com>
2023-02-19 08:39:44 -0300

    Fix problem wit error returning in NewClient

--- src/pages/NewClient.tsx
+++ src/pages/NewClient.tsx
@@ -30,7 +30,7 @@ export const NewClient = () => {
     const apellido = e.currentTarget.clientLastname;
     const tel = e.currentTarget.clientTel;
 
-    if (!nombre.length && !apellido.length && !tel.length) {
+    if (nombre.length <= 0 && apellido.length <= 0 && tel.length <= 0) {
       toast.error("Completa todos los campos");
       return;
     }
