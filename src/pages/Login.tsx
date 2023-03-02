20e144205d8e97daf2562515620d4a2dc3507f42
Facundo Acosta <facundoacostayl@outlook.com>
2023-03-02 07:16:45 -0300

    Add new regex verification in Register and Login pages email inputs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,10 +23,29 @@ export const Login = () => {
     password: "",
   });
 
+  const sanitizeValue = (value: string, name: string) => {
+    let newValue = "";
+
+    if (name === "email") {
+      newValue = value.replace(
+        /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i,
+        ""
+      );
+    }
+
+    if (newValue.length > 0) return newValue;
+    return value;
+  };
+
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const cleanValue = sanitizeValue(
+      e.currentTarget.value,
+      e.currentTarget.name
+    );
+
     setUserData({
       ...userData,
-      [e.currentTarget.name]: e.currentTarget.value,
+      [e.currentTarget.name]: cleanValue,
     });
   };
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -27,9 +27,16 @@ export const Register = () => {
 
   const sanitizeValue = (value: string, name: string) => {
     let newValue = "";
+
     if (name === "userName") {
       newValue = value.replace(/[^a-zA-Z\s]/g, "");
-      console.log(`value = ${value} - name = ${name}`);
+    }
+
+    if (name === "email") {
+      newValue = value.replace(
+        /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i,
+        ""
+      );
     }
 
     if (newValue.length > 0) return newValue;
@@ -41,6 +48,7 @@ export const Register = () => {
       e.currentTarget.value,
       e.currentTarget.name
     );
+
     setUserData({
       ...userData,
       [e.currentTarget.name]: cleanValue,
